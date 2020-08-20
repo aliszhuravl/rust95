540f7a69783c791307fb4b7ec113fc17874e96a1
aliszhuravl <alis.zhuravl@gmail.com>
2020-08-20 13:51:43 +0300

    20/08

--- web/js/main.js
+++ web/js/main.js
@@ -419,4 +419,11 @@ $(document).ready(function() {
         $("body").removeClass('stop');
     });
 
+    $("#call_search").click(function () {
+        $(".search_block").addClass('visible_search');
+    });
+
+    $("#cross-search").click(function () {
+        $(".search_block").removeClass('visible_search');
+    });
 });
